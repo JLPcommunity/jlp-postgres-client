33469d632ec373b51f0553d43c9319b9d04e0a7a
Sigit Prayoga <prayoga.sigit@gmail.com>
2017-02-14 16:52:54 +0700

    Add status label when do request

--- client-ng2/src/app/app.component.ts
+++ client-ng2/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
@@ -6,19 +6,29 @@ import { Http } from '@angular/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   data = [];
+  status = '';
 
   constructor(private http: Http) {
+  }
+
+  ngOnInit() {
     this.getData();
   }
 
   getData() {
+    this.status = 'Requesting...';
     this.http.get('http://localhost:8383/get').subscribe(response => {
       console.log(response);
       this.data = response.json();
     }, error => {
       console.log(error);
+      // Get the 'cause' from error body to be the status
+      var body = error.json()
+      if(body != null){
+        this.status = body.cause;
+      }
     });
   }
 }
